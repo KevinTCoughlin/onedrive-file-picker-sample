6135e8fbef76f871bf66beb93a2b5bc55aed8056
Kevin Coughlin <KevinTCoughlin@users.noreply.github.com>
2019-03-16 14:06:58 -0700

    Initial demo bits

---
+++ README.md
@@ -0,0 +1,8 @@
+# OneDrive File Picker | Embed Preview Demo
+
+Web app demonstrating usage of the OneDrive File Picker to pick document(s) that are then rendered inline as an embed preview.
+
+## Running locally
+
+1. `npm install -g http-server
+1. `http-server .`
---
+++ js/main.js
@@ -0,0 +1,68 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+$(function () {
+    var $buttonEl = $('#launchPicker');
+    var $iframeEl = $('#embed');
+
+    function handlePreviewResponse(response) {
+        let previewUrl = response.getUrl || '';
+
+        $iframeEl[0].src = previewUrl;
+    }
+
+    function postData(url = ``, accessToken = '') {
+        return fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${accessToken}`
+            },
+            // body: JSON.stringify(data),
+        })
+        .then(response => response.json());
+    }
+
+    function getEmbedUrl(endpoint, driveId, itemId) {
+        return `${endpoint}/drives/${driveId}/items/${itemId}/preview`;
+    }
+
+    function onSuccess(response) {
+        console.log('success', response);
+
+        let accessToken = response.accessToken;
+        let apiEndpoint = response.apiEndpoint || 'https://graph.microsoft.com/v1.0/';
+        let file = response.value[0] || {};
+        let fileId = file.id || '';
+        let parentDrive = file.parentReference || {};
+        let driveId = parentDrive.driveId || '';
+
+        let postUrl = getEmbedUrl(apiEndpoint, driveId, fileId);
+
+        postData(postUrl, accessToken)
+            .then(handlePreviewResponse)
+            .catch(onError);
+    }
+
+    function onCancel() {
+        console.log('Action canceled');
+    }
+
+    function onError(err) {
+        console.error(err);
+    }
+
+    function launchPicker() {
+        var options = {
+            clientId: 'bb11e7d9-afdb-4c20-b282-80b9c60a7bd1',
+            accountSwitchEnabled: false,
+            success: onSuccess,
+            cancel: onCancel,
+            error: onError
+        };
+
+        OneDrive.open(options);
+    }
+
+    $buttonEl.on('click', launchPicker);
+});
